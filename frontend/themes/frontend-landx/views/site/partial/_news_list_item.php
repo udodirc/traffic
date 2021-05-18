def3fd3766c75457dfa2063f7baa2ca03e5bb64f
Salmin Lev <udodirc@yahoo.com>
2021-05-18 21:35:32 +0600

    Bugs fixing

--- frontend/themes/frontend-landx/views/site/partial/_news_list_item.php
+++ frontend/themes/frontend-landx/views/site/partial/_news_list_item.php
@@ -4,7 +4,6 @@ use yii\helpers\HtmlPurifier;
 use yii\helpers\Url;
 ?>
 <div class="card">
-	<img class="card-img-top" src="https://via.placeholder.com/363x363" alt="Card image cap">
 	<div class="card-body">
 		<h4 class="card-title mt-3"><?= Html::a(Html::encode($model->title), \Yii::$app->request->BaseUrl.'/news/'.$model->id, []); ?></h4>
 		<p class="card-text"><?= HtmlPurifier::process($model->short_text); ?></p>
