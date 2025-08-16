da8163772fbc4b9452dd95b16f95e63746215334
Salmin Lev <prostoweb123@gmail.com>
2025-08-16 10:44:00 +0600

    Create console command.

---
+++ console/config/console.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    'id' => 'app-console',
+    'basePath' => dirname(__DIR__), // mlm/console
+    'bootstrap' => ['log'],
+    'controllerNamespace' => 'console\controllers',
+    'components' => [
+        'log' => [
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+    ],
+];
