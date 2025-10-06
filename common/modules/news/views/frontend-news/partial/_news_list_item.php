406e1b61da0df18aacd572fc1b1026ae84364a9f
Salmin Lev <prostoweb123@gmail.com>
2025-10-06 18:03:19 +0600

    Fix news.

--- common/modules/news/views/frontend-news/partial/_news_list_item.php
+++ common/modules/news/views/frontend-news/partial/_news_list_item.php
@@ -2,5 +2,6 @@
     <div class="card-body">
         <h4 class="card-title mt-3"><?= $model->title; ?></h4>
         <p class="card-text"><?= $model->short_text; ?></p>
+        <a href="/news/<?= $model->id; ?>">Подробнее</a>
     </div>
 </div>
